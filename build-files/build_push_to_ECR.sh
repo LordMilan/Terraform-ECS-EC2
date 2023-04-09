8b8331e8db416dab0a5bbfa826140c2192f8b300
LordMilan <milan.mahat.739@gmail.com>
2023-04-09 14:01:52 +0545

    build file udpated

--- build-files/build_push_to_ECR.sh
+++ build-files/build_push_to_ECR.sh
@@ -1,8 +1,7 @@
 #cd ./build-files
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 551804445178.dkr.ecr.ap-south-1.amazonaws.com
-sudo git clone -b test-qa git@github.com:CloudTechService/enrollible-msg-center-frontend.git
-cd enrollible-msg-center-frontend
-echo "VUE_APP_API_BASE=https://qa-api-message.enrollible.com/api/v1\nVUE_APP_API_SSO=https://qa-api-sso.enrollible.com/api\nVUE_APP_SSO=https://qa-sso.enrollible.com\nVUE_APP_SITE_KEY=fzVrDNpRAxufNFsWWtfN7aAf2LsCEkvMgIiCXWlf" > .env
-sudo docker build -t qa-enrollible-msg-center-ui .
-sudo docker tag qa-enrollible-msg-center-ui:latest 551804445178.dkr.ecr.ap-south-1.amazonaws.com/qa-ui:qa-enrollible-msg-center-ui
-sudo docker push 551804445178.dkr.ecr.ap-south-1.amazonaws.com/qa-ui:qa-enrollible-msg-center-ui
+sudo git clone https://github.com/gech4me/tic-tac-react.git
+cd tic-tac-react
+sudo docker build -t tik-tac-app .
+sudo docker tag tik-tac-app:latest 551804445178.dkr.ecr.ap-south-1.amazonaws.com/qa-ui:tik-tac-app
+sudo docker push 551804445178.dkr.ecr.ap-south-1.amazonaws.com/qa-ui:tik-tac-app
