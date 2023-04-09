7fcc0eded0c254597cc0c65cc7799f88d19be8f8
LordMilan <milan.mahat.739@gmail.com>
2023-04-09 14:10:12 +0545

    repo updated

--- build-files/build_push_to_ECR.sh
+++ build-files/build_push_to_ECR.sh
@@ -1,7 +1,7 @@
 #cd ./build-files
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 551804445178.dkr.ecr.ap-south-1.amazonaws.com
 sudo git clone git@github.com:LordMilan/tick-tac-toe.git
-cd tic-tac-react
+cd tick-tac-toe
 sudo docker build -t tik-tac-app .
 sudo docker tag tik-tac-app:latest 551804445178.dkr.ecr.ap-south-1.amazonaws.com/qa-ui:tik-tac-app
 sudo docker push 551804445178.dkr.ecr.ap-south-1.amazonaws.com/qa-ui:tik-tac-app
